f09b6f2aae42e910c648e23e955e4589409a8867
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 20:11:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code here
+let dodger = document.getElementById("dodger");
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
