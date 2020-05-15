6a4a0db12e66c8cd4fa79c3eb34afe5a5f42f9cd
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 20:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+ function moveDodgerRight() {
+     let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+ }
