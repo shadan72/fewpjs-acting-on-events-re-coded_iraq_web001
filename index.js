ae11a0621268bcf2c91f144e671faad6a8565404
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 20:10:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Your code here
 document.addEventListener("keydown", function(e) {
-  console.log(e.key);
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
 });
